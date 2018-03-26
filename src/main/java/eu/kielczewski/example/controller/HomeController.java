118b5b3db54658f029a3107f973b613bea5151a5
Andriy Nikolyenko <andrey@bonfire.com>
2018-03-26 11:51:51 +0300

    added principal autopopulating with @AuthenticationPrincipal

--- src/main/java/eu/kielczewski/example/controller/HomeController.java
+++ src/main/java/eu/kielczewski/example/controller/HomeController.java
@@ -2,8 +2,11 @@ package eu.kielczewski.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
@@ -11,8 +14,9 @@ public class HomeController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(@AuthenticationPrincipal Principal p) {
         LOGGER.debug("Getting home page");
+        LOGGER.error("Principal is: " + p);
         return "home";
     }
 
